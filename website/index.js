9d30fb21b26ec161e69f3dc57f8b4d7174f6f2d3
NicolaVivante <57539526+NicolaVivante@users.noreply.github.com>
2022-04-14 21:48:34 +0200

    added base element to home page

--- website/index.js
+++ website/index.js
@@ -1,10 +1,62 @@
-import { init, getAuthManager } from "./init.js";
+import { init, getAuthManager, getDBManager } from "./init.js";
 init();
 
 const authManager = getAuthManager();
+const dbManager = getDBManager();
 const userAvatar = document.getElementById("userAvatar");
 const loginButton = document.getElementById("loginButton");
 const logoutButton = document.getElementById("logoutButton");
+const filterButton = document.getElementById("filterButton");
+const reverseFilter = document.getElementById("reverseFilter");
+
+function login() {
+    window.location.href = "authentication.html";
+}
+
+function logout() {
+    authManager.logout();
+    window.location.href = "index.html";
+}
+
+async function updateLocations() {
+    let locations = await getAllLocations();
+    // clear location list
+    for (let location of locations) {
+        console.log(`${location.name}, average score: ${location.getAvgScore()}, number of reviews: ${location.reviewsCount}`);
+        // render location (create element and insert into the document)
+    }
+}
+
+async function getAllLocations() {
+    let locations = await dbManager.getAllLocations();
+    const filter = getFilter();
+    const reverse = reverseFilter.checked;
+    console.log("getting all locations with filter " + filter + ", reverse: " + reverse);
+    switch (filter) {
+        case "letter": {
+            dbManager.orderLocationsByName(locations, reverse);
+            break;
+        }
+        case "score": {
+            dbManager.orderLocationsByScore(locations, reverse);
+            break;
+        }
+        case "revCount": {
+            dbManager.orderLocationsByReviewsCount(locations, reverse);
+            break;
+        }
+    }
+    return locations;
+}
+
+function getFilter() {
+    let filters = document.getElementsByName("filter");
+    for (let filter of filters) {
+        if (filter.checked) {
+            return filter.value;
+        }
+    }
+}
 
 function hide(element) {
     element.style.display = "none";
@@ -17,7 +69,9 @@ function show(element) {
 userAvatar.onclick = login;
 loginButton.onclick = login;
 logoutButton.onclick = logout;
+filterButton.onclick = updateLocations;
 
+updateLocations();
 if (authManager.getCurrentUser() != null) {
     console.log("Logged");
     hide(loginButton);
@@ -30,12 +84,4 @@ if (authManager.getCurrentUser() != null) {
     console.log("Not logged");
 }
 
-export function login() {
-    window.location.href = "authentication.html";
-}
-
-export function logout() {
-    authManager.logout();
-    window.location.href = "index.html";
-}
 
