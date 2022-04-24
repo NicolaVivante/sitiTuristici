5ef059a0dda7955f9f3dc22301388fc50a125434
NicolaVivante <57539526+NicolaVivante@users.noreply.github.com>
2022-04-24 21:12:39 +0200

    testing

--- index.js
+++ index.js
@@ -6,13 +6,14 @@ const fdbManager = getFirebaseDBManager();
 const authManager = getAuthManager();
 const storageManager = getStorageManager();
 
-let id = "-N-bSnXf0YkX73JOB9Nn";
-let location = await dbManager.getLocation(id, false, false);
-await fdbManager.setLocation(id, location);
-//await fdbManager.removeLocation(id);
-let fLocation = await fdbManager.getLocation(id, true, true);
-console.log(location);
-console.log(fLocation);
+// let id = "-N-bSnXf0YkX73JOB9Nn";
+// let location = await dbManager.getLocation(id, false, false);
+// await fdbManager.setLocation(id, location);
+// await fdbManager.removeLocation(id);
+// let fLocation = await fdbManager.getLocation(id, true, true);
+// console.log(location);
+// console.log(fLocation);
 
-let a = await fdbManager.addLocation(location);
-console.log(a);
+//let a = await fdbManager.addLocation(location);
+//console.log(a);
+storageManager.downloadFile();
